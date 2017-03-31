e826606fe1c229a0154758b17627a5475f7388b4
Yu-Kan <yukan.lai@outlook.com>
2017-03-31 17:48:51 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
   }
 };
