f76030936a8180713a3d1c6904c445904cb8e353
Yu-Kan <yukan.lai@outlook.com>
2017-04-07 18:18:28 +0200

    update till section webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'react', 'lodash', 'redux', 'react-redux', 'react-dom', 'faker', 'react-input-range', 'redux-form', 'redux-thunk'
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
@@ -13,7 +21,23 @@ module.exports = {
         use: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    /** CommonsChunkPlugin tells Webpack to look at (the total sum of) all of our project files between our entry points.
+    * If any modules included in those trees or those dependency structures are identical or of their copies are duplicate between them,
+    * pull them out and only add them to the 'vendor' entry point.
+    **/
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 };
