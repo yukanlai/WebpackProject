1eeb447c05a5187adda85fe197da9b7da7b9dc0a
Yu-Kan <yukan.lai@outlook.com>
2017-04-10 17:12:25 +0200

    add api

--- src/router.js
+++ src/router.js
@@ -7,6 +7,35 @@ import ArtistDetail from './components/artists/ArtistDetail';
 import ArtistCreate from './components/artists/ArtistCreate';
 import ArtistEdit from './components/artists/ArtistEdit';
 
+const componentRoutes = {
+  component: Home,
+  path: '/',
+  indexRoute: { component: ArtistMain },
+  childRoutes: [
+    {
+      path: 'artists/new',
+      getComponent(location, cb){
+        // When Webpack sees 'System.import' in our codebase, it will automatically modify the bundle
+        // that was generated to split off a second bundle or subbundle.
+        System.import('./components/artists/ArtistCreate').then(module => cb(null, module.default));
+        // the first arguement of callback takes error Object 
+      }
+    },
+    {
+      path: 'artists/:id',
+      getComponent(location, cb){
+        System.import('./components/artists/ArtistDetail').then(module => cb(null, module.default));
+      }
+    },
+    {
+      path: 'artists/:id/edit',
+      getComponent(location, cb){
+        System.import('./components/artists/ArtistEdit').then(module => cb(null, module.default));
+      }
+    }
+  ]
+};
+
 const Routes = () => {
   return (
     <Router history={hashHistory}>
